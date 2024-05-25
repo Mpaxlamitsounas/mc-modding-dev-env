c69b0b4b50248c0491de39d79463154b47fe8edb
mpaxlamitsounas <worldstudy123@gmail.com>
2024-05-25 19:26:45 +0300

    Add obsidian material

---
+++ src/main/java/men/mpaxlamitsounas/mplearn/ModMaterials.java
@@ -0,0 +1,10 @@
+package men.mpaxlamitsounas.mplearn;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.common.util.EnumHelper;
+
+public class ModMaterials {
+
+    public static final Item.ToolMaterial OBSIDIAN = EnumHelper.addToolMaterial("materialObsidian", 4, 2000, 12.0F,
+            20.0F - 4.0F, 100);
+}
