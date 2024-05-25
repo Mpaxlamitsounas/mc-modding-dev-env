64cc945c95c7f7370faa4c79a1f2340da3198077
mpaxlamitsounas <worldstudy123@gmail.com>
2024-05-25 20:54:33 +0300

    Spotless

--- src/main/java/men/mpaxlamitsounas/mplearn/items/MSword.java
+++ src/main/java/men/mpaxlamitsounas/mplearn/items/MSword.java
@@ -1,29 +1,25 @@
 package men.mpaxlamitsounas.mplearn.items;
 
+import java.util.HashMap;
+
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.effect.EntityLightningBolt;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.event.entity.EntityStruckByLightningEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import men.mpaxlamitsounas.mplearn.ModMaterials;
 import men.mpaxlamitsounas.mplearn.Tags;
 
-import java.util.HashMap;
-import java.util.HashSet;
 @Mod.EventBusSubscriber(Side.SERVER)
 public class MSword extends ItemSword {
 
     public static HashMap<EntityLivingBase, Long> immuneToLightningDuration = new HashMap<EntityLivingBase, Long>();
+
     public MSword() {
         super(ModMaterials.OBSIDIAN);
         this.setTranslationKey(Tags.MODID + ".msword").setRegistryName("msword");
@@ -31,8 +27,7 @@ public class MSword extends ItemSword {
 
     @Override
     public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
-
-        target.world.addWeatherEffect( new EntityLightningBolt(target.world, target.posX, target.posY, target.posZ,
+        target.world.addWeatherEffect(new EntityLightningBolt(target.world, target.posX, target.posY, target.posZ,
                 true));
         stack.damageItem(1, attacker);
         return true;
