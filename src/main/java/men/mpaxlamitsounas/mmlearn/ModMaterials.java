a10e1a0c33396d38b16f6d27762d70045dbd0be9
mpaxlamitsounas <worldstudy123@gmail.com>
2024-05-26 19:37:11 +0300

    JEI side thing, can't do anything about it, also happens in GTCEu

--- src/main/java/men/mpaxlamitsounas/mmlearn/ModMaterials.java
+++ src/main/java/men/mpaxlamitsounas/mmlearn/ModMaterials.java
@@ -7,7 +7,6 @@ import net.minecraftforge.common.util.EnumHelper;
 
 public class ModMaterials {
 
-    // TODO: figure out why it doesn't appear in JEI
     public static final Item.ToolMaterial OBSIDIAN = EnumHelper.addToolMaterial("materialObsidian", 4, 2000, 12.0F,
             10.0F - 4.0F, 100).setRepairItem(new ItemStack(Item.getItemFromBlock(Blocks.OBSIDIAN)));
 }
