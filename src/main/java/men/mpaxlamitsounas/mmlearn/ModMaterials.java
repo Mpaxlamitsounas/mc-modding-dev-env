ff56ec1c706239a59be6459f05f0a8bf060acc8b
mpaxlamitsounas <worldstudy123@gmail.com>
2024-05-26 19:25:16 +0300

    Make sword repairable with obsidian

--- src/main/java/men/mpaxlamitsounas/mmlearn/ModMaterials.java
+++ src/main/java/men/mpaxlamitsounas/mmlearn/ModMaterials.java
@@ -1,10 +1,13 @@
 package men.mpaxlamitsounas.mmlearn;
 
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.util.EnumHelper;
 
 public class ModMaterials {
 
+    // TODO: figure out why it doesn't appear in JEI
     public static final Item.ToolMaterial OBSIDIAN = EnumHelper.addToolMaterial("materialObsidian", 4, 2000, 12.0F,
-            10.0F - 4.0F, 100);
+            10.0F - 4.0F, 100).setRepairItem(new ItemStack(Item.getItemFromBlock(Blocks.OBSIDIAN)));
 }
